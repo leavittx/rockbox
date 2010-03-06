588b9fd8dfc593ac30d12a06cec58c08089c579d
Lev <lev.panov@gmail.com>
2010-03-06 19:48:03 +0300

    * sierpinski: added some new curves

--- apps/plugins/sierpinski.c
+++ apps/plugins/sierpinski.c
@@ -34,8 +34,8 @@
 PLUGIN_HEADER
 
 #define SLEEP_TIME 3
-#define MAX_ITER 7
-#define SQRT3 1.7320508075689
+//#define MAX_ITER 9
+#define MAX_ITER 20
 #define SQRT3BY2 0.86602540378444
 
 #define DOT(a, b)	((a).x * (b).x + (a).y * (b).y)
@@ -92,7 +92,396 @@ void color_apply(struct Color * color, struct screen * display)
 }
 #endif
 
-void DrawIter(int Iter, struct vec2 p1, struct vec2 p2, int dir, struct screen *display)
+/* Draw an iteration of triangle effect */
+void DrawIterT(int Iter, struct vec2 p1, struct vec2 p2, int dir, struct screen *display)
+{
+    struct Color color;
+    
+    if (Iter < 0)
+        return;
+
+    /* Set color to red.
+     * TODO: iteration-depending color handling */
+    color.r = 255;
+    color.g = color.b = 0;
+    color_apply(&color, display);
+        
+    if (Iter == 0)
+        display->drawline(p1.x, p1.y, p2.x, p2.y);
+  
+    struct vec2 s = {p2.x - p1.x, p2.y - p1.y};
+    struct vec2 m = {(p1.x + p2.x) / 2, (p1.y + p2.y) / 2};
+    struct vec2 n = {(p2.y - p1.y) * dir, (p1.x - p2.x) * dir};
+    double len;
+
+    /* We need to normalize normal vector. */
+    NORMALIZE(n);
+    
+    len = rb_sqrt(s.x * s.x + s.y * s.y) / 2;
+    n.x *= len;
+    n.y *= len;
+    
+    m.x += n.x;
+    m.y += n.y;
+    
+    DrawIterT(Iter - 1, p1, m, dir * (-1), display);
+    DrawIterT(Iter - 1, m, p2, dir * (-1), display);
+}
+
+/* Draw an iteration of Lévy C curve */
+void DrawIterLC(int Iter, struct vec2 p1, struct vec2 p2, int dir, struct screen *display)
+{
+    struct Color color;
+    
+    if (Iter < 0)
+        return;
+
+    /* Set color to red.
+     * TODO: iteration-depending color handling */
+    color.r = 255;
+    color.g = color.b = 0;
+    color_apply(&color, display);
+        
+    if (Iter == 0)
+        display->drawline(p1.x, p1.y, p2.x, p2.y);
+  
+    struct vec2 s = {p2.x - p1.x, p2.y - p1.y};
+    struct vec2 m = {(p1.x + p2.x) / 2, (p1.y + p2.y) / 2};
+    struct vec2 n = {(p2.y - p1.y) * (-dir), (p1.x - p2.x) * (-dir)};
+    double len;
+
+    /* We need to normalize normal vector. */
+    NORMALIZE(n);
+    
+    len = rb_sqrt(s.x * s.x + s.y * s.y) / 2;
+    n.x *= len;
+    n.y *= len;
+    
+    m.x += n.x;
+    m.y += n.y;
+    
+    DrawIterLC(Iter - 1, p1, m, dir, display);
+    DrawIterLC(Iter - 1, m, p2, dir, display);
+}
+
+/* Draw an iteration of dragon curve */
+void DrawIterDC(int Iter, struct vec2 p1, struct vec2 p2, int dir, struct screen *display)
+{
+    struct Color color;
+    
+    if (Iter < 0)
+        return;
+
+    /* Set color to red.
+     * TODO: iteration-depending color handling */
+    color.r = 255;
+    color.g = color.b = 0;
+    color_apply(&color, display);
+        
+    if (Iter == 0)
+        display->drawline(p1.x, p1.y, p2.x, p2.y);
+  
+    struct vec2 s = {p2.x - p1.x, p2.y - p1.y};
+    struct vec2 m = {(p1.x + p2.x) / 2, (p1.y + p2.y) / 2};
+    struct vec2 n = {(p2.y - p1.y) * dir, (p1.x - p2.x) * dir};
+    double len;
+
+    /* We need to normalize normal vector. */
+    NORMALIZE(n);
+    
+    len = rb_sqrt(s.x * s.x + s.y * s.y) / 2;
+    n.x *= len;
+    n.y *= len;
+    
+    m.x += n.x;
+    m.y += n.y;
+    
+    DrawIterDC(Iter - 1, p1, m, dir, display);
+    DrawIterDC(Iter - 1, m, p2, dir * (-1), display);
+}
+
+/* Draw an iteration of blood effect */
+void DrawIterB(int Iter, struct vec2 p1, struct vec2 p2, int dir, struct screen *display)
+{
+    struct Color color;
+    
+    if (Iter < 0)
+        return;
+
+    /* Set color to red.
+     * TODO: iteration-depending color handling */
+    color.r = 255;
+    color.g = color.b = 0;
+    color_apply(&color, display);
+        
+    if (Iter == 0)
+        display->drawline(p1.x, p1.y, p2.x, p2.y);
+  
+    struct vec2 s = {p2.x - p1.x, p2.y - p1.y};
+    struct vec2 m = {(p1.x + p2.x) / 2, (p1.y + p2.y) / 2};
+    struct vec2 n = {(p2.y - p1.y) * dir, (p1.x - p2.x) * dir};
+    double len = rb_sqrt(s.x * s.x + s.y * s.y) * SQRT3BY2;
+    
+    /* We need to normalize normal vector. */
+    NORMALIZE(n);
+    /*
+    n.x *= len;
+    n.y *= len;
+    
+    m.x += n.x;
+    m.y += n.y;
+    */
+    s.x = (p1.x + p2.x) / 3;
+    s.y = (p1.y + p2.y) / 3;
+    DrawIterB(Iter - 1, p1, s, dir, display);
+    
+    len = rb_sqrt((s.x * s.x + s.y * s.y) / 3) * SQRT3BY2;
+    n.x *= len;
+    n.y *= len;
+    
+    m.x += n.x;
+    m.y += n.y;
+    
+    DrawIterB(Iter - 1, s, m, dir, display);
+    
+    s.x = (p1.x + p2.x) * 2 / 3;
+    s.y = (p1.y + p2.y) * 2 / 3;
+    DrawIterB(Iter - 1, m, s, dir, display);
+    
+    DrawIterB(Iter - 1, s, p2, dir, display);
+}
+
+/* Draw an iteration of blood effect, CurveType 2 */
+void DrawIterB2(int Iter, struct vec2 p1, struct vec2 p2, int dir, struct screen *display)
+{
+    struct Color color;
+    
+    if (Iter < 0)
+        return;
+
+    /* Set color to red.
+     * TODO: iteration-depending color handling */
+    color.r = 255;
+    color.g = color.b = 0;
+    color_apply(&color, display);
+        
+    if (Iter == 0)
+        display->drawline(p1.x, p1.y, p2.x, p2.y);
+  
+    struct vec2 s = {p2.x - p1.x, p2.y - p1.y};
+    struct vec2 m = {(p1.x + p2.x) / 2, (p1.y + p2.y) / 2};
+    struct vec2 n = {(p2.y - p1.y) * dir, (p1.x - p2.x) * dir};
+    double len = rb_sqrt(s.x * s.x + s.y * s.y) * SQRT3BY2;
+    
+    /* We need to normalize normal vector. */
+    NORMALIZE(n);
+    /*
+    n.x *= len;
+    n.y *= len;
+    
+    m.x += n.x;
+    m.y += n.y;
+    */
+    s.x = (p1.x + 0.5*p2.x) / 1.5;
+    s.y = (p1.y + 0.5*p2.y) / 1.5;
+    DrawIterB2(Iter - 1, p1, s, dir, display);
+    
+    len = rb_sqrt((s.x * s.x + s.y * s.y) / 3) * SQRT3BY2;
+    n.x *= len;
+    n.y *= len;
+    
+    m.x += n.x;
+    m.y += n.y;
+    
+    DrawIterB2(Iter - 1, s, m, dir, display);
+    
+    s.x = (p1.x + p2.x) * 2 / 3;
+    s.y = (p1.y + p2.y) * 2 / 3;
+    DrawIterB2(Iter - 1, m, s, dir, display);
+    
+    DrawIterB2(Iter - 1, s, p2, dir, display);
+}
+
+/* Draw an iteration of Koch curve */
+void DrawIterKC(int Iter, struct vec2 p1, struct vec2 p2, int dir, struct screen *display)
+{
+    struct Color color;
+    
+    if (Iter < 0)
+        return;
+
+    /* Set color to red.
+     * TODO: iteration-depending color handling */
+    color.r = 255;
+    color.g = color.b = 0;
+    color_apply(&color, display);
+        
+    if (Iter == 0)
+        display->drawline(p1.x, p1.y, p2.x, p2.y);
+  
+    struct vec2 s = {p2.x - p1.x, p2.y - p1.y};
+    struct vec2 s1;
+    struct vec2 m = {(p1.x + p2.x) / 2, (p1.y + p2.y) / 2};
+    struct vec2 n = {(p2.y - p1.y) * dir, (p1.x - p2.x) * dir};
+    double len;
+
+    s1.x = (p1.x + 0.5*p2.x) / 1.5;
+    s1.y = (p1.y + 0.5*p2.y) / 1.5;
+    DrawIterKC(Iter - 1, p1, s1, dir, display);
+    
+    /* We need to normalize normal vector. */
+    NORMALIZE(n);
+    
+    len = rb_sqrt(s.x * s.x + s.y * s.y) / 3 * SQRT3BY2;
+    n.x *= len;
+    n.y *= len;
+    
+    m.x += n.x;
+    m.y += n.y;
+    DrawIterKC(Iter - 1, s1, m, dir, display);
+    
+    s.x = (p1.x + 2*p2.x) / 3;
+    s.y = (p1.y + 2*p2.y) / 3;
+    DrawIterKC(Iter - 1, m, s, dir, display);
+    DrawIterKC(Iter - 1, s, p2, dir, display);
+}
+
+/* Draw an iteration of flower, CurveType 2 */
+void DrawIterFlower2(int Iter, struct vec2 p1, struct vec2 p2, int dir, struct screen *display)
+{
+    struct Color color;
+    
+    if (Iter < 0)
+        return;
+    
+    /* Set color to red.
+     * TODO: iteration-depending color handling */
+    color.r = 255;
+    color.g = color.b = 0;
+    color_apply(&color, display);
+    
+    struct vec2 s = {p2.x - p1.x, p2.y - p1.y};
+    struct vec2 m = {(p1.x + p2.x) / 2, (p1.y + p2.y) / 2};
+    struct vec2 n = {(p2.y - p1.y) * dir, (p1.x - p2.x) * dir};
+    double len = rb_sqrt(s.x * s.x + s.y * s.y) * SQRT3BY2;
+    
+    /* We need to normalize normal vector. */
+    NORMALIZE(n);
+    
+    n.x *= len;
+    n.y *= len;
+    
+    m.x += n.x;
+    m.y += n.y;
+    
+    if (Iter == 0)
+        display->drawline(p1.x, p1.y, p2.x, p2.y);
+    
+    s.x = (p1.x + m.x) / 2;
+    s.y = (p1.y + m.y) / 2;
+    if (Iter & 1)
+        DrawIterFlower2(Iter - 1, p1, s, dir * (-1), display);
+    else
+        DrawIterFlower2(Iter - 1, p1, s, dir, display);
+    
+    n.x = (p2.x + m.x) / 2;
+    n.y = (p2.y + m.y) / 2;
+    if (Iter & 1)
+    {
+        DrawIterFlower2(Iter - 1, s, n, dir * (-1), display);
+        DrawIterFlower2(Iter - 1, n, p2, dir * (-1), display);
+    }
+    else
+    {
+        DrawIterFlower2(Iter - 1, s, n, dir, display);
+        DrawIterFlower2(Iter - 1, n, p2, dir, display);
+    }
+}
+
+/* Draw an iteration of beautiful flower */
+void DrawIterFlower(int Iter, struct vec2 p1, struct vec2 p2, int dir, struct screen *display)
+{
+    struct Color color;
+    
+    if (Iter < 0)
+        return;
+    
+    /* Set color to red.
+     * TODO: iteration-depending color handling */
+    color.r = 255;
+    color.g = color.b = 0;
+    color_apply(&color, display);
+    
+    struct vec2 s = {p2.x - p1.x, p2.y - p1.y};
+    struct vec2 m = {(p1.x + p2.x) / 2, (p1.y + p2.y) / 2};
+    struct vec2 n = {(p2.y - p1.y) * dir, (p1.x - p2.x) * dir};
+    double len = rb_sqrt(s.x * s.x + s.y * s.y) * SQRT3BY2;
+    
+    /* We need to normalize normal vector. */
+    NORMALIZE(n);
+    
+    n.x *= len;
+    n.y *= len;
+    
+    m.x += n.x;
+    m.y += n.y;
+    
+    if (Iter == 0)
+        display->drawline(p1.x, p1.y, p2.x, p2.y);
+    
+    s.x = (p1.x + m.x) / 2;
+    s.y = (p1.y + m.y) / 2;
+    DrawIterFlower(Iter - 1, p1, s, dir * (-1), display);
+    
+    n.x = (p2.x + m.x) / 2;
+    n.y = (p2.y + m.y) / 2;
+    DrawIterFlower(Iter - 1, s, n, dir * (-1), display);
+    DrawIterFlower(Iter - 1, n, p2, dir * (-1), display);
+}
+
+/* Draw an iteration of Sierpinski triangle CurveType 1 */
+void DrawIterST1(int Iter, struct vec2 p1, struct vec2 p2, int dir, struct screen *display)
+{
+    struct Color color;
+    
+    if (Iter < 0)
+        return;
+
+    /* Set color to red.
+     * TODO: iteration-depending color handling */
+    color.r = 255;
+    color.g = color.b = 0;
+    color_apply(&color, display);
+        
+    if (Iter == 0)
+        display->drawline(p1.x, p1.y, p2.x, p2.y);
+  
+    struct vec2 s = {p2.x - p1.x, p2.y - p1.y};
+    struct vec2 m = {(p1.x + p2.x) / 2, (p1.y + p2.y) / 2};
+    struct vec2 n = {(p2.y - p1.y) * dir, (p1.x - p2.x) * dir};
+    double len = rb_sqrt(s.x * s.x + s.y * s.y) * SQRT3BY2;
+    
+    /* We need to normalize normal vector. */
+    NORMALIZE(n);
+    
+    n.x *= len;
+    n.y *= len;
+    
+    m.x += n.x;
+    m.y += n.y;
+    
+    s.x = (p1.x + m.x) / 2;
+    s.y = (p1.y + m.y) / 2;
+    DrawIterST1(Iter - 1, p1, s, dir * (-1), display);
+    
+    n.x = (p2.x + m.x) / 2;
+    n.y = (p2.y + m.y) / 2;
+    DrawIterST1(Iter - 1, s, n, dir, display);
+    DrawIterST1(Iter - 1, n, p2, dir * (-1), display);
+}
+
+/* Draw an iteration of Sierpinski triangle CurveType 2 */
+void DrawIterST2(int Iter, struct vec2 p1, struct vec2 p2, int dir, struct screen *display)
 {
     struct Color color;
     
@@ -155,15 +544,15 @@ void DrawIter(int Iter, struct vec2 p1, struct vec2 p2, int dir, struct screen *
     
     s.x = (p1.x + m.x) / 2;
     s.y = (p1.y + m.y) / 2;
-    DrawIter(Iter - 1, p1, s, dir * (-1), display);
+    DrawIterST2(Iter - 1, p1, s, dir * (-1), display);
     
     s.x = (p2.x + m.x) / 2;
     s.y = (p2.y + m.y) / 2;
-    DrawIter(Iter - 1, p2, s, dir, display);
+    DrawIterST2(Iter - 1, p2, s, dir, display);
     
     s.x = (m.x + p2.x) / 2;
     s.y = (m.y + p2.y) / 2;
-    DrawIter(Iter - 1, m, s, dir * (-1), display);
+    DrawIterST2(Iter - 1, m, s, dir * (-1), display);
 }
 
 int plugin_main(void)
@@ -173,6 +562,15 @@ int plugin_main(void)
     int W, H;
     int Iter = 0;
     bool need_redraw = true;
+    
+    void(* DrawIter[])(int Iter, struct vec2 p1, struct vec2 p2, int dir, struct screen *display) = {
+        DrawIterST2, DrawIterST1,
+        DrawIterFlower, DrawIterFlower2,
+        DrawIterB, DrawIterB2, DrawIterT,
+        DrawIterKC, DrawIterDC,
+        DrawIterLC
+        };
+    int CurveType = 0;
 	
     FOR_NB_SCREENS(i)
     {
@@ -194,17 +592,39 @@ int plugin_main(void)
             if (need_redraw)
             {
                 struct vec2 p1, p2;
+                
+                if (DrawIter[CurveType] == DrawIterDC)
+                {
+                    p1.x = W - H + 10;
+                    p1.y = H - 70;
+                    
+                    p2.x = W - 10;
+                    p2.y = H - 70;
+                }
+                
+                else if (DrawIter[CurveType] == DrawIterLC)
+                {
+                    p1.x = (W - H) + 5;
+                    p1.y = 70;
+                    
+                    p2.x = H - 5;
+                    p2.y = 70;
+                }
+                
+                else
+                {
+                    p1.x = (W - H) / 2;
+                    p1.y = H - 15;
     
-                p1.x = (W - H) / 2;
-                p1.y = H - 15;
-    
-                p2.x = W - (W - H) / 2;
-                p2.y = H - 15;
+                    p2.x = W - (W - H) / 2;
+                    p2.y = H - 15;
+                }
                 
                 rb->lcd_clear_display();
                 
                 StartIter = Iter;
-                DrawIter(Iter, p1, p2, 1, display);
+                //DrawIterST2(Iter, p1, p2, 1, display);
+                DrawIter[CurveType](Iter, p1, p2, 1, display);
                 need_redraw = false;
                 
                 display->update();
@@ -212,7 +632,7 @@ int plugin_main(void)
 
             rb->sleep(SLEEP_TIME);
 
-            action = pluginlib_getaction(TIMEOUT_NOBLOCK, 
+            action = pluginlib_getaction(TIMEOUT_NOBLOCK,
                                          plugin_contexts,
                                          NB_ACTION_CONTEXTS);
             switch (action)
@@ -235,6 +655,15 @@ int plugin_main(void)
                         Iter--;
                         need_redraw = true;
                     }
+                    break;
+                    
+                case PLA_MENU:
+                    if ((unsigned int)CurveType < ARRAYLEN(DrawIter) - 1)
+                        CurveType++;
+                    else
+                        CurveType = 0;
+                    
+                    need_redraw = true;
                     break;
                     
                 default:
