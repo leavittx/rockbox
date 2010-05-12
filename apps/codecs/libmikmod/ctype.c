ce3853ad27a405437af9431a25f819528bf8b453
Lev <lev.panov@gmail.com>
2010-05-12 23:18:29 +0400

    * change some include paths in mikmod

---
+++ apps/codecs/libmikmod/ctype.c
@@ -0,0 +1,5 @@
+#include "../../../firmware/libc/ctype.c"
+#include "../../../firmware/libc/strncmp.c"
+#include "../../../firmware/common/strcasecmp.c"
+#include "../../../firmware/libc/random.c"
+
