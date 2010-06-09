2f170d472b0cf3f1d5c82c14f523ee8b593df6fc
Lev <lev.panov@gmail.com>
2010-06-09 13:54:34 +0400

    ~ prepare to apply mikmod patch - remove old mikmod files

--- apps/codecs/libmikmod/ctype.c
+++
@@ -1,4 +0,0 @@
-//#include "../../../firmware/libc/ctype.c"
-//#include "../../../firmware/libc/strncmp.c"
-//#include "../../../firmware/common/strcasecmp.c"
-//#include "../../../firmware/libc/random.c"
