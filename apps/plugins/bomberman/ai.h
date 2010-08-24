6a387fb0fac4a564abb1f007bcf793c1b63ac5a2
Lev <lev.panov@gmail.com>
2010-08-24 13:14:47 +0400

    ~ bomberman: add file ai.h (Nick)

---
+++ apps/plugins/bomberman/ai.h
@@ -0,0 +1,46 @@
+/***************************************************************************
+ *             __________               __   ___.
+ *   Open      \______   \ ____   ____ |  | _\_ |__   _______  ___
+ *   Source     |       _//  _ \_/ ___\|  |/ /| __ \ /  _ \  \/  /
+ *   Jukebox    |    |   (  <_> )  \___|    < | \_\ (  <_> > <  <
+ *   Firmware   |____|_  /\____/ \___  >__|_ \|___  /\____/__/\_ \
+ *                     \/            \/     \/    \/            \/
+ * $Id$
+ *
+ * Copyright (C) 2010 Lev Panov, Nick Petrov
+ * 
+ * Bomberman plugin
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This software is distributed on an "AS IS" basis, WITHOUT WARRANTY OF ANY
+ * KIND, either express or implied.
+ *
+ ****************************************************************************/
+
+
+#ifndef _AI_H_
+#define _AI_H_
+
+#include "game.h"
+
+
+typedef struct
+{
+  bool IsWalkable;
+  bool IsOnOpen;
+  bool IsOnClose;
+  int G, H, F;
+  int ParentX, ParentY;
+} NODE;
+
+typedef struct
+{
+  int X, Y;
+  int F;
+} PATHELEM;
+
+#endif /* _AI_H_ */
